2238c30f3a8de6c36ca974df8ae08241271a896e
Achyut <achyut.dev@gmail.com>
2015-02-15 22:42:17 +0545

    Final working script

--- sun-seeker.ino
+++ sun-seeker.ino
@@ -1,77 +1,83 @@
-//#include <Stepper.h>
+//--------------------------Sun-Seeker---------------------------//
+//---------------------------------------------------------------//
 
-int pinin1=A0;
-int pinin2=A1;
+//assigning analog pin to the sensor input 
+int pinin1=A0,pinin2=A1;
+
+//store analog value
+int sensorValue1=0,sensorValue2=0;
+
+//digital out comparing sensor values 
 int stpin1,stpin2;
-const int m1=2;
-const int m2=3;
-const int m3=4;
-const int m4=5;
-const int m5=8;
-const int m6=9;
-int a=0,s=0;
-int sensorValue1=0;
-int sensorValue2=0;
-//const int stepsPerRevolution = 200;
-//Stepper myStepper(stepsPerRevolution, 8,9,10,11); 
-void stop_M();
-void forward_M();
-void stepper_M();
+
+//these digital output for driving three motors 
+const int m1=2,m2=3,m3=4,m4=5,m5=8,m6=9;
+
+//counter parameters
+int interval_to_read_sensor_value=0,loop_count=0;
+
+
+//robot's basic functions
+void stop_right_there();
+void move_forward_searching_light();
+void rotate_in_dir_of_light();
+
 
 void setup()
-  {
-    pinMode(m1,OUTPUT);
-    pinMode(m2,OUTPUT);
-    pinMode(m3,OUTPUT);
-    pinMode(m4,OUTPUT);
-    pinMode(m5,OUTPUT);
-    pinMode(m6,OUTPUT);
- }
+{
+  pinMode(m1,OUTPUT);
+  pinMode(m2,OUTPUT);
+  pinMode(m3,OUTPUT);
+  pinMode(m4,OUTPUT);
+  pinMode(m5,OUTPUT);
+  pinMode(m6,OUTPUT);
+}
+
 void loop()
 {
 
-    sensorValue1 = analogRead(pinin1);    
-    if(sensorValue1<=150)
-         stpin1=HIGH;
-    else
-         stpin1=LOW;
-    sensorValue2 = analogRead(pinin2);    
-    if(sensorValue2<=150)
-         stpin2=HIGH; 
-    else
-         stpin2=LOW;
+  sensorValue1 = analogRead(pinin1);    
+  if(sensorValue1<=150)
+    stpin1=HIGH;
+  else
+    stpin1=LOW;
+  sensorValue2 = analogRead(pinin2);    
+  if(sensorValue2<=150)
+    stpin2=HIGH; 
+  else
+    stpin2=LOW;
 
-// 1st condition both sensor detect light
-   if (stpin1==HIGH && stpin2==HIGH) 
-   {
-     stop_M();
-   }
-//2nd condition both sensor doesnot detect light
-   else if(stpin1==LOW && stpin2== LOW)
-   {
-     forward_M();
-    }
-//3rd condition one of the sensor detect light
-   else
-      {
-          stepper_M();
-      }
-if(a==500)
-{
-  digitalWrite(m1,LOW);
-  digitalWrite(m2,HIGH);
-  digitalWrite(m3,LOW);
-  digitalWrite(m4,LOW);
-  delay(4300);
-  a=0;
-  s++;
-  stop_M();
+  // 1st condition both sensor detect light
+  if (stpin1==HIGH && stpin2==HIGH) 
+  {
+    stop_right_there();
+  }
+  //2nd condition both sensor doesnot detect light
+  else if(stpin1==LOW && stpin2== LOW)
+  {
+    move_forward_searching_light();
+  }
+  //3rd condition one of the sensor detect light
+  else
+  {
+    rotate_in_dir_of_light();
+  }
+  if(interval_to_read_sensor_value==500)
+  {
+    digitalWrite(m1,LOW);
+    digitalWrite(m2,HIGH);
+    digitalWrite(m3,LOW);
+    digitalWrite(m4,LOW);
+    delay(4300);
+    interval_to_read_sensor_value=0;
+    loop_count++;
+    stop_right_there();
+  }
+  if(loop_count==4)
+    exit(0);
 }
-if(s==4)
-exit(0);
 
-}
-void stop_M()
+void stop_right_there()
 {
   digitalWrite(m1,LOW);
   digitalWrite(m2,LOW);
@@ -81,27 +87,29 @@ void stop_M()
   digitalWrite(m6,LOW);
   // delay(50);
   return;
-  
+
 }
-void forward_M()
+
+void move_forward_searching_light()
 {
   digitalWrite(m1,LOW);
   digitalWrite(m2,HIGH);
   digitalWrite(m3,LOW);
   digitalWrite(m4,HIGH);
   delay(20);
-  a++;
-  stop_M();
+  interval_to_read_sensor_value++;
+  stop_right_there();
   return;
 }
-void stepper_M()
+
+void rotate_in_dir_of_light()
 {
   if(stpin1==HIGH && stpin2==LOW)
   {
     digitalWrite(m5,HIGH);
     digitalWrite(m6,LOW);  
     delay(50);
-    stop_M();
+    stop_right_there();
     return;
   }
   else
@@ -109,9 +117,10 @@ void stepper_M()
     digitalWrite(m5,LOW);
     digitalWrite(m6,HIGH);
     delay(50);
-    stop_M();
+    stop_right_there();
     return;
   } 
 }
 
- 
+
+
